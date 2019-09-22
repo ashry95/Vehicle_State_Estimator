d7eab2ae53bf45190168e3b949636bdcb81a40f1
Ahmed Ashry <a.elashry95@gmail.com>
2019-09-22 22:29:01 +0200

    First Commit

---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,35 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../uart_echo_ccs.cmd 
+
+C_SRCS += \
+../startup_ccs.c \
+../uart_Eecho.c 
+
+C_DEPS += \
+./startup_ccs.d \
+./uart_Eecho.d 
+
+OBJS += \
+./startup_ccs.obj \
+./uart_Eecho.obj 
+
+OBJS__QUOTED += \
+"startup_ccs.obj" \
+"uart_Eecho.obj" 
+
+C_DEPS__QUOTED += \
+"startup_ccs.d" \
+"uart_Eecho.d" 
+
+C_SRCS__QUOTED += \
+"../startup_ccs.c" \
+"../uart_Eecho.c" 
+
+
---
+++ Debug/uart_Eecho.d
@@ -0,0 +1,55 @@
+# FIXED
+
+uart_Eecho.obj: ../uart_Eecho.c
+uart_Eecho.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdint.h
+uart_Eecho.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/_stdint40.h
+uart_Eecho.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/stdint.h
+uart_Eecho.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/cdefs.h
+uart_Eecho.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_types.h
+uart_Eecho.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_types.h
+uart_Eecho.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_stdint.h
+uart_Eecho.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_stdint.h
+uart_Eecho.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdbool.h
+uart_Eecho.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h
+uart_Eecho.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h
+uart_Eecho.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h
+uart_Eecho.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h
+uart_Eecho.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h
+uart_Eecho.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h
+uart_Eecho.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h
+uart_Eecho.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_uart.h
+uart_Eecho.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h
+uart_Eecho.obj: C:/ti/TivaWare_C_Series-2.1.4.178/utils/uartstdio.h
+uart_Eecho.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdarg.h
+uart_Eecho.obj: C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h
+uart_Eecho.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h
+uart_Eecho.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/fpu.h
+uart_Eecho.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/rom.h
+uart_Eecho.obj: C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/timer.h
+
+../uart_Eecho.c: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdint.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/_stdint40.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/stdint.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/cdefs.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_types.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_types.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/machine/_stdint.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/sys/_stdint.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdbool.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/debug.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_uart.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/uart.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/utils/uartstdio.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-arm_18.12.1.LTS/include/stdarg.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/inc/hw_ints.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/interrupt.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/fpu.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/rom.h: 
+C:/ti/TivaWare_C_Series-2.1.4.178/driverlib/timer.h: 
